425452acb071c2ca961305ce1b3b1c5f17f71f93
Nam Phong <phongnnhe176274@fpt.edu.vn>
2026-01-16 22:17:16 +0700

    create homepage layot

---
+++ frontend/src/components/features/home/HomeContent.jsx
@@ -0,0 +1,9 @@
+const HomeContent = () => {
+    return (
+        <div>
+            <h1>HomeContent</h1>
+        </div>
+    );
+};
+
+export default HomeContent;
---
+++ frontend/src/components/features/home/HomeNavbar.jsx
@@ -0,0 +1,9 @@
+const HomeNavbar = () => {
+    return (
+        <div>
+            <h1>HomeNavbar</h1>
+        </div>
+    );
+};
+
+export default HomeNavbar;  
