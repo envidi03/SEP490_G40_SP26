b663d2176a33f6a1945c819d57b24f7a9d873a7b
Top <dinhn7933@gmail.com>
2026-01-17 18:49:22 +0700

    login controller

--- backend/src/modules/auth/auth.controller.js
+++ backend/src/modules/auth/auth.controller.js
@@ -12,4 +12,18 @@ exports.register = async (req, res, next) => {
     } catch (error) {
         next(error);
     }
-} 
+}
+
+exports.login = async (req, res, next) => {
+    try {
+        const data = req.body;
+        const result = await authService.login(data);
+        res.status(200).json({
+            success: true,
+            message: 'User logged in successfully',
+            data: result
+        })
+    } catch (error) {
+        next(error);
+    }
+}
