d24367502616f9775d3bfed18b03cfa100084884
Nam Phong <phongnnhe176274@fpt.edu.vn>
2026-01-18 12:43:43 +0700

    create pages Register

--- frontend/src/routes/publicRoutes.jsx
+++ frontend/src/routes/publicRoutes.jsx
@@ -1,4 +1,5 @@
 import Login from '../pages/auth/Login';
+import Register from '../pages/auth/Register';
 import HomePage from '../pages/home_page/HomePage';
 
 const publicRoutes = [
@@ -9,6 +10,10 @@ const publicRoutes = [
   {
     path: "login",
     element: <Login />
+  },
+  {
+    path: "register",
+    element: <Register />
   }
 ];
 
