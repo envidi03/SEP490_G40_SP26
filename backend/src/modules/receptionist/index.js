4b0a9e0c87fbb615ac7897de6a92dbadfd997d84
Nguyen Van Dinh <dinhn7933@gmail.com>
2026-03-09 12:17:16 +0700

    get invoice stats feature ver2

--- backend/src/modules/receptionist/index.js
+++ backend/src/modules/receptionist/index.js
@@ -1,3 +1,11 @@
 const route = require('./route/dashboard.route');
+const DashboardService = require('./service/dashboard.service');
+const DashboardController = require('./controller/dashboard.controller');
+const DashboardModel = null; // Dashboard ko dùng model riêng mà query từ nhiều models khác
 
-module.exports = { route };
+module.exports = {
+    route,
+    DashboardService,
+    DashboardController,
+    DashboardModel
+};
