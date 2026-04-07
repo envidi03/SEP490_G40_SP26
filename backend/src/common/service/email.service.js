f8d14a19c6528583ebc74a04af96b32fc68bedca
Tran Trung Toan <trantrungtoan17092003@gmail.com>
2026-04-08 00:38:35 +0700

    fix: enable secure connection for email transport in EmailService

--- backend/src/common/service/email.service.js
+++ backend/src/common/service/email.service.js
@@ -7,7 +7,7 @@ class EmailService {
         this.transporter = nodemailer.createTransport({
             host: process.env.SMTP_HOST,
             port: process.env.SMTP_PORT,
-            secure: false,
+            secure: true,
             auth: {
                 user: process.env.SMTP_USER,
                 pass: process.env.SMTP_PASS
